1e004d527fde4fae721c1d8b789f4c87bf70f448
Elias May <elias.may13@gmail.com>
2020-10-01 01:49:58 -0500

    Finish Game class

--- main.py
+++ main.py
@@ -138,26 +138,40 @@ class Game:
         self.p1.printAll()
         print("P2:")
         self.p2.printAll()
-    def fight(self, wager=[]):
+    def fight(self, wager=None):
+        if wager is None: 
+            wager = []
+        #print("wagerStart", len(wager))
         if not (self.playing):
             raise Exception("This game is over")
-        card1 = self.p1.pop()[0]
+        card1 = self.p1.pop()
         if(card1=="Loss"):
-            return "p2"   
-        card2 = self.p2.pop()[0]
+            return "p2"  
+        card1 = card1[0] 
+        card2 = self.p2.pop()
         if(card2=="Loss"):
             return "p1" 
+        card2 = card2[0] 
         wager.append(card1)
         wager.append(card2)
         if(card1.compare(card2)=="Less"):
+            #print("wager", len(wager))
+            for x in wager:
+                if type(x) != Card:
+                    raise Exception("Card in wager error")
             self.p2.add(wager)
-            return "done"
+            return "ok"
         if(card1.compare(card2)=="Greater"):
+            #print("wager", len(wager))
             self.p1.add(wager)
-            return "done"
+            for x in wager:
+                if type(x) != Card:
+                    raise Exception("Card in wager error")
+            return "ok"
         if(card1.compare(card2)=="Equal"):
             return self.war(wager)
     def war(self, wager):
+        #print("war")
         p1wager = self.p1.pop(3)
         if(p1wager=="Loss"):
             return "p2"
@@ -167,8 +181,17 @@ class Game:
         wager += p1wager
         wager += p2wager
         return self.fight(wager)
+    def playAll(self):
+        status = 'ok'
+        while(status == 'ok'):
+            #g.printAll()
+            self.turns += 1
+            status = self.fight()
+        return [status, self.turns]
+
 
 g = Game()
-g.printAll()
-print(g.fight())
-g.printAll()
+#g.printAll()
+##print(g.fight())
+#g.printAll()
+print(g.playAll())
